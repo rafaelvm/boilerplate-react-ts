0e8f7488c74fee54ec25553e4a760280694fa755
Rafael Vieira <77240308+rafaelvm@users.noreply.github.com>
2022-05-27 15:27:35 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <GlobalStyle />
       <div style={{ textAlign: "center", marginTop: 80 }}>
-        <h1>Boileplate React with Typescript</h1>
+        <h1>Boilerplate React with Typescript</h1>
         <h4 style={{ fontWeight: 400, marginTop: 10 }}>come be happy :D</h4>
       </div>
     </>
